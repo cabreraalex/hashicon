9887e14934d63084b54d3eff4fb9518cb3656b0e
Alex Cabrera <alex.cabrera@gmail.com>
2015-06-09 19:05:19 -0400

    structured for PyPi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'hashicon',
+    packages = ['hashicon'],
+    version = '0.1',
+    description = 'Create minimalistic icons from a string',
+    author = 'Alex Cabrera',
+    author_email = 'alex.cabrera@gmail.com',
+    url = 'https://github.com/cabreraalex/hashicon',
+    download_url = 'https://github.com/cabreraalex/hashicon/tarball/0.1',
+    keywords = ['icon', 'PIL', 'hash', 'picture'],
+    classifiers = [],
+)
+
+
