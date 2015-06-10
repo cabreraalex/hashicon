50305eee7f3baf0c9c7e71f3c791b22ad58f8ed8
Alex Cabrera <alex.cabrera@gmail.com>
2015-06-09 20:19:32 -0400

    fixing execution

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'hashicon',
     packages = ['hashicon'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'Create minimalistic icons from a string',
     author = 'Alex Cabrera',
     author_email = 'alex.cabrera@gmail.com',
     url = 'https://github.com/cabreraalex/hashicon',
     download_url = 'https://github.com/cabreraalex/hashicon/tarball/0.1',
-    keywords = ['icon', 'PIL', 'hash', 'picture'],
+    license='MIT',
+    install_requires=['Pillow']
+    keywords = ['icon', 'PIL', 'hash', 'picture', 'Pillow'],
     classifiers = [],
+    entry_points = {'console_scripts': [
+        'hashicon = hashicon.hashicon:main',
+        ]
+    }
 )
 
 
